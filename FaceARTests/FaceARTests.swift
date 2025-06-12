eac11de451d6901ca6a66ab2d351cf7a9681a78d
prammmoe <ikhwanpramuditha05@gmail.com>
2025-06-12 09:47:13 +0700

    Initial Commit

---
+++ FaceARTests/FaceARTests.swift
@@ -0,0 +1,17 @@
+//
+//  FaceARTests.swift
+//  FaceARTests
+//
+//  Created by Pramuditha Muhammad Ikhwan on 12/06/25.
+//
+
+import Testing
+@testable import FaceAR
+
+struct FaceARTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
