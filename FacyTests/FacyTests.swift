0ad178fc243b5bc0c48f6a11f63aafb65a24fb71
prammmoe <ikhwanpramuditha05@gmail.com>
2025-06-14 11:39:39 +0700

    Initial Commit

---
+++ FacyTests/FacyTests.swift
@@ -0,0 +1,17 @@
+//
+//  FacyTests.swift
+//  FacyTests
+//
+//  Created by Pramuditha Muhammad Ikhwan on 14/06/25.
+//
+
+import Testing
+@testable import Facy
+
+struct FacyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
